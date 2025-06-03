bd89f34258e29a881da351cbe6b691dada383ffe
Surjeet Kumar <115608471+Surjeet4146@users.noreply.github.com>
2025-06-03 17:11:40 +0530

    Create index.js

---
+++ backend/src/models/index.js
@@ -0,0 +1,9 @@
+const Transaction = require('./Transaction');
+const Alert = require('./Alert');
+const Address = require('./Address');
+
+module.exports = {
+  Transaction,
+  Alert,
+  Address
+};
